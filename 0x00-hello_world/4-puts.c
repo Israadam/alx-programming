f90435a20fb5f87537664535d2a34a57aac5b9c5
israadam <israaosama796#gmail.com>
2023-09-08 07:12:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+<stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Description:Calls puts and recieves value returned by puts after its
+ * Description: Calls puts and recieves value returned by puts after its
  * execution.
- * Return:Always return 0 to indicate success
+ * Return: Always return 0 to indicate success
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle');
-	return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
