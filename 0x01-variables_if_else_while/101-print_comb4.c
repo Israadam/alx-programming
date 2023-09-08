917dd0583164c6706c8ff15adafb54079cf87dcc
israadam <israaosama796#gmail.com>
2023-09-08 16:40:34 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Printd sll possible diffrent combination of these digits
+ * main - Prints all possible diffrent combination of these digits
  * Return: Always 0 (Success)
  */
 int main(void)
