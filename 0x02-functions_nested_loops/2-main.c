185cd70ccba4546ca4f97c9808bc64323724658c
israadam <israaosama796#gmail.com>
2023-09-12 11:29:39 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * main - check the code
- *
+ *void print_alphabet_x10(void);
  * Return: Always 0
  */
-.int main(void)
+int main(void)
 {
         print_alphabet_x10();
         return (0);
