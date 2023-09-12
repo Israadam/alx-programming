550f0f8faa01994698becfe9e6c95e7d6f1897fc
israadam <israaosama796#gmail.com>
2023-09-12 11:48:06 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,15 @@
  *
  * Return: 0
  */
-int main(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	char j;
+
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 'a' ; j <= 'z' ; j++)
 			_putchar(j);
-		putchar('\n');
 	}
+	_putchar('\n');
 }
