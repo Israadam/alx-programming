5e14ddd67e62c3c2a4e1bdc8678a22aa7d2378f8
israadam <israaosama796#gmail.com>
2023-09-14 17:03:37 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if(b >= a && b >= c)
+	else if (b >= a && b >= c)
 	{
-		largest =b;
+		largest = b;
 	}
 	else
 	{
 		largest = c;
 	}
-	
 	return (largest);
 }
