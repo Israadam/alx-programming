a2f1b2b971ac6fafc46f6da76fe09cba355841aa
israadam <israaosama796@gmail.com>
2023-10-24 05:10:36 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,21 +11,21 @@
  * Return: pointers to the new list
  */
 
-const listint_t **_r(const listint_t **list, size_t size, const listint_t*new)
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
-	const listint_t **newlist;
-	size_t i;
-	newlist = malloc(size * sizeof(listint_t));
-	if(newlist == NULL)
-	{
-		free(list);
-		exit(99);
-	}
-	for (i = 0; i < size -1; i++)
-		newlist[i] = list[i];
-	newlist[i] = new;
-	free(list);
-	return (newlist);
+const listint_t **newlist;
+size_t i;
+newlist = malloc(size * sizeof(listint_t));
+if (newlist == NULL)
+{
+free(list);
+exit(99);
+}
+for (i = 0; i < size - 1; i++)
+newlist[i] = list[i];
+newlist[i] = new;
+free(list);
+return (newlist);
 }
 
 /**
@@ -35,24 +35,25 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t*new)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, num = 0;
-	const listint_t **list = NULL;
-	while (head != NULL)
-	{
-		for (i = 0; i < num; i++)
-		{
-			if (head == list[i])
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free(list);
-				return (num);
-			}
-		}
-		num++;
-		list = _r(list, num, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-	}
-	free(list);
-	return (num);
+size_t i, num = 0;
+const listint_t **list = NULL;
+while (head != NULL)
+
+{
+for (i = 0; i < num; i++)
+{
+if (head == list[i])
+{
+printf("-> [%p] %d\n", (void *)head, head->n);
+free(list);
+return (num);
+}
+}
+num++;
+list = _r(list, num, head);
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+free(list);
+return (num);
 }
